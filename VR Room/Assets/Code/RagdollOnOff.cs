f016c8ab823a44038fdf33e037da5cfc10a316cf
MichaelKrawiec <xxpapu37xx@gmail.com>
2022-11-29 23:10:22 +0000

    Demo_12
    Ragdoll

---
+++ VR Room/Assets/Code/RagdollOnOff.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollOnOff : MonoBehaviour
+{
+    public BoxCollider mainCollider;
+    public GameObject rig;
+    public Animator animator;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        GetRagdollBits();
+        ragdollOff();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "sword")
+        {
+            ragdollOn();
+            Debug.Log("RagdollOn");
+        }
+    }
+
+    public Collider[] ragdollColliders;
+    Rigidbody[] limbsRigidbodies;
+    void GetRagdollBits()
+    {
+        ragdollColliders = rig.GetComponentsInChildren<Collider>();
+        limbsRigidbodies = rig.GetComponentsInChildren<Rigidbody>();
+    }
+
+    void ragdollOn()
+    {
+        animator.enabled = false;
+
+        foreach (Collider col in ragdollColliders)
+        {
+            col.enabled = true;
+        }
+
+        foreach (Rigidbody rigid in limbsRigidbodies)
+        {
+            rigid.isKinematic = false;
+        }
+
+        
+        mainCollider.enabled = false;
+        rb.isKinematic = true;
+    }
+
+    void ragdollOff()
+    {
+        foreach (Collider col in ragdollColliders) {
+            col.enabled = false;
+        }
+
+        foreach (Rigidbody rigid in limbsRigidbodies)
+        {
+            rigid.isKinematic = true;
+        }
+
+        animator.enabled = true;
+        mainCollider.enabled = true;
+        rb.isKinematic = false;
+    }
+}
