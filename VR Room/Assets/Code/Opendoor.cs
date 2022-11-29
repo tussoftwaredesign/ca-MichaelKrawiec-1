11017a5f5212ace5e97da98d5de07efc45f86c6e
MichaelKrawiec <90252845+MichaelKrawiec@users.noreply.github.com>
2022-11-29 11:03:28 +0000

    Demo_10

--- VR Room/Assets/Code/Opendoor.cs
+++ VR Room/Assets/Code/Opendoor.cs
@@ -6,16 +6,37 @@ public class Opendoor : MonoBehaviour
 {
     public GameObject Door;
     public Rigidbody rb;
+    public Animator animator;
 
+    bool allInActive = true;
+    public GameObject[] myObjects;
     // Start is called before the first frame update
     void Start()
     {
         rb = Door.GetComponent<Rigidbody>();
     }
 
+    public void check()
+    {
+        for (int i = 0; i < myObjects.Length; i++)
+        {
+            if (!myObjects[i].activeSelf)
+            {
+                allInActive = true;
+                break; //this "quits" the for loop, you don't need to check the rest of the items if you already found one that's not active
+            }
+        }
+
+        if (allInActive)
+            //GetComponent<Animator>().enabled = false;
+            moveDoor();
+    }
+
+
     public void moveDoor()
     {
-        Vector3 newRotation = new Vector3(0, 10, 0);
+        Vector3 newRotation = new Vector3(0, -85, 0);
         transform.eulerAngles = newRotation;
+        Debug.Log("Opening Door!");
     }
 }
